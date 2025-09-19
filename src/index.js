19d6250a6abc963d99c5b079e70e55344f517572
Amitabh Kale <amitabhkale@gmail.com>
2025-09-19 16:36:14 +0530

    Server Initialized with express and dotenv

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv");
+const app = require("./app");
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on PORT ${PORT}`);
+});
