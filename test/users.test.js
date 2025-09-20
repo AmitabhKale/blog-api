3d1b35e5d2410c750d4ec231a050e03321087ef0
Amitabh Kale <amitabhkale@gmail.com>
2025-09-20 22:43:50 +0530

    Login User Test Case and mocking user data

--- test/users.test.js
+++ test/users.test.js
@@ -1,14 +1,42 @@
 const request = require("supertest");
+const bcrypt = require("bcryptjs");
 
 const app = require("../src/app");
+const User = require("../src/models/userModel");
+
+function hashPassword(password) {
+  const hash = bcrypt.hashSync(password, 8);
+  return hash;
+}
+
+const user = {
+  name: "Luffy",
+  email: "luffy@pirates.com",
+  password: hashPassword("kaizoku"),
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(user).save();
+});
 
 test("Should Register a new user", async () => {
   await request(app)
     .post("/api/users/register")
     .send({
-      name: "Luffy",
-      email: "luffy@priates.com",
-      password: "nikuniku",
+      name: "Zoro",
+      email: "zoro@pirates.com",
+      password: "enmasake",
     })
     .expect(201);
+});
+
+test("Should Login User existing in database", async () => {
+  await request(app)
+    .post("/api/users/login")
+    .send({
+      email: user.email,
+      password: "kaizoku",
+    })
+    .expect(200);
 });
