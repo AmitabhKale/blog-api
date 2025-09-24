3d0466e17793b6a8bcf775ef4002db04e0ff4f7f
Amitabh Kale <amitabhkale@gmail.com>
2025-09-24 22:18:46 +0530

    Blogs data for insertion into db

---
+++ src/config/blogsData.js
@@ -0,0 +1,14 @@
+// Dummy Data to use for testing and inserting into DB
+const blogsData = [
+  {
+    title: "Nodejs Fundamentals for working professionals",
+    description:
+      "Nodejs is a JavaScript Runtime Enviroment, However few topics are there like events, streams,crypto and fs modules need to be mastered",
+    categories: ["development", "javascript", "programming"],
+  },
+  {
+    title: "Understannding Docker for Developers",
+    description: "Docker is a platform to build, ship and deploy",
+    categories: ["development", "javascript", "programming"],
+  },
+];
